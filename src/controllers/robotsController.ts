e813e6d45d4067bb4aa07dfa4a96887c29732c82
Fernando Di Masi <wildiney@gmail.com>
2021-10-11 09:41:27 -0300

    bugfix: typo

--- src/controllers/robotsController.ts
+++ src/controllers/robotsController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express'
+
+const rules: string = `User-agent: *
+Disallow: /
+`
+
 export const robots = (request: Request, response: Response) => {
   response.type('text/plain')
-  response.send(
-    `User-agent: *
-    Disallow: /`
-  )
+  response.send(rules)
 }
